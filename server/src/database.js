7e5a92d0fe283617d46e894be26b55d55cbf731c
Balazs <martikanbalazs@gmail.com>
2021-12-21 02:29:41 +0100

    Adding more sample data

--- server/src/database.js
+++ server/src/database.js
@@ -41,7 +41,8 @@ export async function seedDatabase(){
       },
       { 
         
-        "title": "The Emperor's Handbook: A New Translation of The Meditations", "descriptions:" : "Victor Davis HansonProfessor of Classics at California State UniversityThis new, accessible translation by Scot and David Hicks of the emperor's famous Stoic handbook reflects far better the flavor of Marcus Aurelius's own style. Americans should read Marcus -- and this new edition now makes it a joy.",
+        "title": "The Emperor's Handbook: A New Translation of The Meditations", 
+        "descriptions" : "Victor Davis HansonProfessor of Classics at California State UniversityThis new, accessible translation by Scot and David Hicks of the emperor's famous Stoic handbook reflects far better the flavor of Marcus Aurelius's own style. Americans should read Marcus -- and this new edition now makes it a joy.",
         "externalLink": "https://www.amazon.com/Emperors-Handbook-New-Translation-Meditations/dp/0743233832/ref=sr_1_1?crid=1X1KPT3UHTPRJ&keywords=the+emperors+handbook&qid=1639750619&sprefix=Emperors+hand%2Caps%2C229&sr=8-1"
       },
       { 
@@ -52,8 +53,26 @@ export async function seedDatabase(){
       },
       { 
         
-        "title": "RESPAWN RSP-110 Racing Style Gaming, Reclining Ergonomic Chair with Footrest, Green", "descriptions:" : "GAMIFIED SEATING: A racecar-style gaming chair that provides luxury and comfort, whether it's used for intense gaming sessions and climbing to the top of the leaderboards, or long work days.",
-        "externalLink": "https://www.amazon.com/RESPAWN-110-Racing-Style-Gaming-Chair/dp/B076HTJRMZ/ref=sr_1_6?keywords=gaming+chairs&pd_rd_r=32bdbf43-408e-4b73-8b35-0101a287d119&pd_rd_w=jlGHM&pd_rd_wg=dycjL&pf_rd_p=12129333-2117-4490-9c17-6d31baf0582a&pf_rd_r=71SV3FYPX39GTGZDD5R4&qid=1639750773&sr=8-6"
+        "title": "The Rational Male - Preventive Medicine", 
+        "descriptions" : "Building on the core works of The Rational Male – Preventive Medicine presents a poignant outline of the phases of maturity and the most commonly predictable experiences men can expect from women as they progress through various stages of life.",
+        "externalLink": "https://www.amazon.de/-/en/dp/1508596557/?coliid=I3NM4PK98F6OD8&colid=A1YH98MXIJ38&psc=1&ref_=lv_ov_lig_dp_it"
+      },
+      { 
+        
+        "title": "Slick Gorilla Hair Styling Texturising Powder 20 g", 
+        "descriptions" : "Use Slick Gorilla hair styling powder for anti-gravity lift with a matte finish. Give your hair texture, style and volume.",
+        "externalLink": "https://www.amazon.de/-/en/Slick-Gorilla-Styling-Texturising-Powder/dp/B06XZNJWZL/ref=pd_day0_2/260-1583002-7551952?pd_rd_w=JqSZt&pf_rd_p=a4a39702-3e52-471f-bb6a-2dbd6f139cc7&pf_rd_r=RBX9YG35DFXJGRWJ31BA&pd_rd_r=73a3e3ec-9bbc-4562-85e1-066de84b03b5&pd_rd_wg=nlb5y&pd_rd_i=B06XZNJWZL&psc=1"
+      },
+      { 
+        
+        "title": "2021 Apple MacBook Pro", 
+        "descriptions" : "2021 Apple MacBook Pro (16, Apple M1 Pro Chip mit 10‑Core CPU und 16‑Core GPU, 16 GB RAM, 512 GB SSD) - Space Grey .",
+        "externalLink": "https://www.amazon.de/-/en/MK183D-A/dp/B09JQZZRZ7/ref=sr_1_5?crid=2CSG4V7FQK018&keywords=apple+macbook+pro+16gb+ram+2021&qid=1640049882&sprefix=apple+macbook+pro+16gb+ram+2021%2Caps%2C80&sr=8-5"
+      },
+      { 
+        "title": "iRobot Roomba e5 (e5154) Robot Vacuum Cleaner", 
+        "descriptions" : "Three-stage cleaning power – even with stubborn dirt, the cleaning system is finished with 3 stages. Two rubber brushes for all floors and extra strong suction power with 5 times more suction power.",
+        "externalLink": "https://www.amazon.de/-/en/e5154-Cleaner-3-Level-Cleaning-Brushes/dp/B07FQNN2MB/ref=sr_1_4?crid=35G4SM35EMCZJ&keywords=irobot&qid=1640049984&sprefix=irobot%2Caps%2C87&sr=8-4"
       }
     ];
     await Wish.insertMany(defaultWish);
